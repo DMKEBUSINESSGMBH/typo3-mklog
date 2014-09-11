2ea9091fd25dda65c3b5c537794fde6f5070f446
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.de>
2014-09-11 12:30:05 +0200

    typo3 dependecy gefixed

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,38 +1,49 @@
 <?php
-/**
+
+/***************************************************************
  * Extension Manager/Repository config file for ext "mklog".
- */
+ *
+ * Auto generated 11-09-2014 12:29
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'MK Logging',
-  	'description' => 'Keep track of developer logs. Provides automatic email notify about important errors.',
-  	'category' => 'be',
-  	'author' => 'René Nitzsche, Michael Wagner, Hannes Bochmann',
-  	'author_company' => 'DMK E-BUSINESS',
-  	'author_email' => 'dev@dmk-ebusiness.de',
-  	'shy' => '',
-  	'dependencies' => '',
-  	'conflicts' => '',
-  	'priority' => '',
-  	'module' => '',
-  	'state' => 'alpha',
-  	'internal' => '',
-  	'uploadfolder' => 0,
-  	'modify_tables' => '',
-  	'clearCacheOnLoad' => 0,
-  	'lockType' => '',
-  	'version' => '1.1.0',
-  	'constraints' => array(
-    	'depends' => array(
-      		'rn_base' => '0.10.3-0.0.0',
-      		'devlog' => '',
-      		'scheduler' => '',
-    		'typo3' => '4.3.0-',
+	'description' => 'Keep track of developer logs. Provides automatic email notify about important errors.',
+	'category' => 'be',
+	'author' => 'René Nitzsche, Michael Wagner, Hannes Bochmann',
+	'author_company' => 'DMK E-BUSINESS',
+	'author_email' => 'dev@dmk-ebusiness.de',
+	'shy' => '',
+	'dependencies' => 'rn_base,devlog,scheduler',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'version' => '1.1.0',
+	'constraints' => array(
+		'depends' => array(
+			'rn_base' => '0.10.3-0.0.0',
+			'devlog' => '',
+			'scheduler' => '',
+			'typo3' => '4.3.0-6.2.99',
 		),
-    	'conflicts' => array(
-    	),
-    	'suggests' => array(
-    	),
-  	),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'suggests' => array(
+	),
+	'_md5_values_when_last_written' => 'a:15:{s:16:"ext_autoload.php";s:4:"ff56";s:17:"ext_localconf.php";s:4:"d951";s:14:"ext_tables.sql";s:4:"7ac6";s:16:"locallang_db.xml";s:4:"fcaa";s:10:"README.txt";s:4:"c13b";s:26:"Documentation/Includes.txt";s:4:"ef74";s:23:"Documentation/Index.rst";s:4:"74aa";s:26:"Documentation/Settings.yml";s:4:"a775";s:33:"Documentation/ChangeLog/Index.rst";s:4:"2259";s:38:"Documentation/Images/SchedulerTask.png";s:4:"d16c";s:36:"Documentation/Introduction/Index.rst";s:4:"3ad1";s:35:"Documentation/UsersManual/Index.rst";s:4:"1353";s:47:"scheduler/class.tx_mklog_scheduler_WatchDog.php";s:4:"e76f";s:63:"scheduler/class.tx_mklog_scheduler_WatchDogAddFieldProvider.php";s:4:"3dad";s:35:"srv/class.tx_mklog_srv_WatchDog.php";s:4:"8769";}',
 );
 
+?>
