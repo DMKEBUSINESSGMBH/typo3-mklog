560f69de75e6eecd1f4040726a8d72c56435aa51
Mario Seidel <mario.seidel@dmk-ebusiness.de>
2016-11-22 12:33:39 +0100

    fix requirements and drop TYPO3 4.5 support

--- ext_emconf.php
+++ ext_emconf.php
@@ -33,7 +33,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'depends' => array(
 			'rn_base' => '1.1.4-0.0.0',
 			'scheduler' => '',
-			'typo3' => '4.3.0-7.6.99',
+			'typo3' => '6.2.0-7.6.99',
 		),
 		'conflicts' => array(
 		),
