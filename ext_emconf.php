a222538c5e89ff2fea79e87d562565d81ffa33c9
Michael Wagner <michael.wagner@dmk-ebusiness.de>
2017-04-05 10:42:34 +0200

    #6 typo3-cms dependency increased to 8.7

--- ext_emconf.php
+++ ext_emconf.php
@@ -33,7 +33,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'depends' => array(
 			'rn_base' => '1.1.4-0.0.0',
 			'scheduler' => '',
-			'typo3' => '6.2.0-7.6.99',
+			'typo3' => '6.2.0-8.7.99',
 		),
 		'conflicts' => array(
 		),
