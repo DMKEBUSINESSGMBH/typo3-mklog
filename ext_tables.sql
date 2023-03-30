df167d0db3a0389c0daeafd6a67142e379999667
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.com>
2023-03-30 10:19:03 +0200

    Cleanup remove obsolete field
    fixes #23

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,11 +1,4 @@
 #
-# Table structure for table 'tx_scheduler_task'
-#
-CREATE TABLE tx_scheduler_task (
-    tx_mklog_lastrun datetime DEFAULT '0000-00-00 00:00:00',
-);
-
-#
 # Table structure for table 'tx_mklog_devlog'
 #
 CREATE TABLE tx_mklog_devlog_entry (
