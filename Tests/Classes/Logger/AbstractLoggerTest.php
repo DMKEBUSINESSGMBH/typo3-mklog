9b4bce1e5985a63c213ca5b0a53983d92f49f13d
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.com>
2024-12-16 11:38:11 +0100

    Bugfix optimize test
    from time to time the test fails because
    there is too much delay between lock file content
    generation in the data provider and
    the execution of the check

--- Tests/Classes/Logger/AbstractLoggerTest.php
+++ Tests/Classes/Logger/AbstractLoggerTest.php
@@ -68,12 +68,16 @@ class AbstractLoggerTest extends \DMK\Mklog\Tests\BaseTestCase
      */
     protected $lockFile;
 
+    protected AbstractLogger $abstractLogger;
+
     protected function setUp(): void
     {
         parent::setUp();
         $lockFilesFolder = Environment::getVarPath().'/lock/';
         GeneralUtility::mkdir_deep($lockFilesFolder);
         $this->lockFile = $lockFilesFolder.'mklog_exception_during_logging.lock';
+
+        $this->abstractLogger = $this->getMockForAbstractClass(AbstractLogger::class);
     }
 
     protected function tearDown(): void
@@ -89,10 +93,8 @@ class AbstractLoggerTest extends \DMK\Mklog\Tests\BaseTestCase
      */
     public function testCreateDevlogEntry(): void
     {
-        $abstractLogger = $this->getAccessibleMock(AbstractLogger::class);
-
         $logEntry = $this->callInaccessibleMethod(
-            $abstractLogger,
+            $this->abstractLogger,
             'createDevlogEntry',
             'This is a {placeholder_one} {placeholder_two}',
             'mklog',
@@ -113,27 +115,28 @@ class AbstractLoggerTest extends \DMK\Mklog\Tests\BaseTestCase
      *
      * @dataProvider canMailBeSendDataProvider
      */
-    public function canMailBeSend(?int $lockFileContent, bool $canMailBeSend): void
+    public function canMailBeSend(?int $timeOffset, bool $canMailBeSend): void
     {
-        if (!is_null($lockFileContent)) {
-            file_put_contents($this->lockFile, $lockFileContent);
+        if (!is_null($timeOffset)) {
+            file_put_contents($this->lockFile, time() - $timeOffset);
         }
 
-        $abstractLogger = $this->getAccessibleMock(AbstractLogger::class);
-
-        self::assertSame($canMailBeSend, $this->callInaccessibleMethod($abstractLogger, 'canMailBeSend'));
+        self::assertSame($canMailBeSend, $this->callInaccessibleMethod($this->abstractLogger, 'canMailBeSend'));
     }
 
     public static function canMailBeSendDataProvider(): array
     {
         return [
             [null, true],
-            [1, true],
-            [time(), false],
-            [time() - 50, false],
-            [time() - 59, false],
-            [time() - 60, false],
-            [time() - 61, true],
+            [time(), true],
+            [time() + 1, true],
+            [time() + 100, true],
+            [0, false],
+            [50, false],
+            [59, false],
+            [60, false],
+            [61, true],
+            [610, true],
         ];
     }
 
@@ -144,13 +147,12 @@ class AbstractLoggerTest extends \DMK\Mklog\Tests\BaseTestCase
      */
     public function getExceptionTraceWithoutArguments(): void
     {
-        $abstractLogger = $this->getAccessibleMock(AbstractLogger::class);
         try {
             throw new \Exception('An error occured');
         } catch (\Exception $exception) {
             self::assertStringNotContainsString(
                 'Object(DMK\Mklog\Logger\AbstractLoggerTest)',
-                $this->callInaccessibleMethod($abstractLogger, 'getExceptionTraceWithoutArguments', $exception)
+                $this->callInaccessibleMethod($this->abstractLogger, 'getExceptionTraceWithoutArguments', $exception)
             );
         }
     }
